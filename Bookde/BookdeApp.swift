f8e649ff506c4edfc8a1aaa8202a593a3799db60
Kien Pham <kienluproductions@gmail.com>
2022-11-14 10:06:27 +0700

    Initial Commit

---
+++ Bookde/BookdeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookdeApp.swift
+//  Bookde
+//
+//  Created by Kevin on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct BookdeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
